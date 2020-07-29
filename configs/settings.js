df4446869d77529c0f487776423a712e58f2b586
xingjin <jinjin_xing@qingsongchou.com>
2020-07-29 20:03:03 +0800

    修改host

--- configs/settings.js
+++ configs/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   SYSTEMLOGPATH: "/home/logsdir/api-wx-express", // 服务器日志保存目录
 
   // 数据库配置
-  HOST: "39.107.27.66",
+  HOST: "http://39.106.22.71",
   PORT: 27017,
   DB: "wx-muse"
 };
